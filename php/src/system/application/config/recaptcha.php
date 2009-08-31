297bcad3d0021a86ecfee88cb26402b7fbdea79e
Alastair Fraser Dewar <alastair@AllysNetbook.(none)>
2009-08-31 20:40:05 +0100

    Time for a clean slate

--- php/src/system/application/config/recaptcha.php
+++
@@ -1,8 +0,0 @@
-$config['recaptcha'] = array(
-public=> '6Lc7FAUAAAAAALVbrYrMFH71Qq0BBLhp94Izcgwv ',
-private=> '6Lc7FAUAAAAAAJ6OD_QB-6tbZlzJVkx0sUcFDffm ',
-RECAPTCHA_API_SERVER => 'http://api.recaptcha.net',
-RECAPTCHA_API_SECURE_SERVER=> 'https://api-secure.recaptcha.net',
-RECAPTCHA_VERIFY_SERVER => 'api-verify.recaptcha.net',
-theme => 'red'
-);
