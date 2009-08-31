caac9fc4ed055bd2c9d027570b78d1f74055b56f
AlastairDewar <Alastair@alastairdewar.co.uk>
2009-08-31 21:07:27 +0100

    Some more pseudowork

--- library/Building.php
+++ library/Building.php
@@ -1,11 +1,31 @@
 <?php
 class Building {
 
+   /*
+    *   The buildings unique identifier
+    */
    var $unique = 0;
+   /*
+    *   The buildings name
+    */
    var $name = "";
+   /*
+    *   A short description of the building
+    */
    var $description = "";
 
+   /*
+    *   Links to ResourceRequirement objects related to the building
+    */
    var $resources_required = array();
+   /*
+    *   Links to ReseachRequirement objects related to the building
+    */   
+   var $research_required = array();
+   /*
+    *   Links to ResourceGeneration objects related to the building
+    */
+   var $resources_generated = array();
 
    function Building($ini_unique, $ini_name, $ini_description) {
 	# Change these to set when set and gets are done
@@ -20,6 +40,26 @@ class Building {
 
    function __destruct() {
 	
+   }
+
+   function add_resource_requirement() {
+	# Check existing requirements for duplicates
+	# Attempt to add to database
+   }
+
+   function remove_resource_requirement() {
+	# Check existing requirements
+	# Attempt to remove from database
+   }
+
+   function add_research_requirement() {
+	# Check existing requirements for duplicates
+	# Attempt to add to database
+   }
+
+   function remove_research_requirement() {
+	# Check existing requirements
+	# Attempt to remove from database
    }
 
 }
