1aeeccc8bf9ce501fe8ef6dc84d844a05ed08c9a
AlastairDewar <Alastair@alastairdewar.co.uk>
2009-09-01 18:27:46 +0100

    Done some more pseudowork. Also moved some stuff from Building to PlayerBuilding. It did fit but needs some thinking over.

--- library/Building.php
+++ library/Building.php
@@ -14,19 +14,6 @@ class Building {
     */
    var $description = "";
 
-   /*
-    *   Links to ResourceRequirement objects related to the building
-    */
-   var $resources_required = array();
-   /*
-    *   Links to ReseachRequirement objects related to the building
-    */   
-   var $research_required = array();
-   /*
-    *   Links to ResourceGeneration objects related to the building
-    */
-   var $resources_generated = array();
-
    function Building($ini_unique, $ini_name, $ini_description) {
 	# Change these to set when set and gets are done
 	$this->unique = $ini_unique;
@@ -40,26 +27,6 @@ class Building {
 
    function __destruct() {
 	
-   }
-
-   function add_resource_requirement() {
-	# Check existing requirements for duplicates
-	# Attempt to add to database
-   }
-
-   function remove_resource_requirement() {
-	# Check existing requirements
-	# Attempt to remove from database
-   }
-
-   function add_research_requirement() {
-	# Check existing requirements for duplicates
-	# Attempt to add to database
-   }
-
-   function remove_research_requirement() {
-	# Check existing requirements
-	# Attempt to remove from database
    }
 
 }
