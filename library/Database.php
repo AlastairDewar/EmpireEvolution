d675a19d915700b8263e2a0a6ec56d276f35fa3d
Alastair Dewar <Alastair@allysdesktop.(none)>
2009-11-05 15:40:40 +0000

    Started work on the library

--- library/Database.php
+++ library/Database.php
@@ -12,8 +12,8 @@ class Database {
    function __construct() {
 	print "Initiating database connection\n";
 	# Create database connection	
-		#$this->connection = mysqli_connect($hostname, $username, $password);
-		#mysqli_select_db($this->connection, $name);
+		$this->connection = mysqli_connect($hostname, $username, $password);
+		mysqli_select_db($this->connection, $name);
    }
 
    function __destruct() {
@@ -24,7 +24,7 @@ class Database {
    function query($query) {
 	if($this->connection != null){
 	$this->query_string = mysqli_query($query);}else{
-	print "Failure to estalish database connecton first\n";}
+	print "Failure to establish database connection first\n";}
    }
 
    function fetch_array() {
@@ -32,7 +32,7 @@ class Database {
 	if($this->query_string != null){
 	return mysqli_fetch_array($this->query_string);}else{
 	print "Failure to find existing query\n";}}else{
-	print "Failure to estalish database connecton first\n";}
+	print "Failure to establish database connection first\n";}
    }
 
 }
--- library/Player.php
+++ library/Player.php
@@ -6,29 +6,57 @@ class Player {
     */
    var $unique = 0;
    /*
-    *   The players username
+    *   The players unique username
     */
    var $username = "";
    /*
-    *   Links to PlayerBuilding objects related to the player
+    *   The players unique email
     */
-   var $buildings = array();
+   var $email = "";
+   /*
+    *   An array of town owned by a player
+    */
+   var $towns = array();
 
-   function __construct() {
+   function __construct($uid = 0) {
+    // Check for existing player
 	print "Loading player\n";
-	$this->load_player_buildings();
+	$this->load_towns();
    }
 
    function __destruct() {
 	print "Logging out player\n";
    }
 
-   function load_player_buildings() {
-	# Loop through
-	# Create new building
-		# $current_building = new PlayerBuilding();
-	# Add to array
-		# array_push($buildings, $current_building);
+   function load_towns() {
+	// Loop through database searching for towns owned by the player
+	// Create new town object from database and add buildings, resources etc.
+   }
+   
+   function get_uid() {
+   	return $this->unique;
+   }
+   
+   function get_username() {
+   	return $this->username;
+   }
+   
+   function set_username($new_username) {
+   	// Check for formatting
+   	// Check for exclusivity
+   	// Update database
+   	$this->username = $new_username;
+   }
+   
+   function get_email() {
+   	return $email;
+   }
+   
+   function set_email($email = "") {
+   	// Check for formatting
+   	// Check for exclusivity
+   	// Send new confirmation email
+   	// Add to email change request table in db
    }
 
 }
